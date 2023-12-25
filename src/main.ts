a6c9c818f80d3d4899ad3b853e7b0fdac6afe966
André Teixeira <andre-x8@hotmail.com>
2023-12-25 12:59:57 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('Serviço rodando na porta 3000');
 }
 bootstrap();
