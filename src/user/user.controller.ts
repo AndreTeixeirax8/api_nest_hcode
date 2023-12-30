19836170fc15f9a87d486b18cdba868cd0eac605
André Teixeira <andre-x8@hotmail.com>
2023-12-30 15:17:48 -0300

    adicionado update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,37 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post()
   async create(@Body() body) {
     return {};
+  }
+
+  @Get()
+  async read() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async readOne(@Param() params) {
+    return { users: {}, params };
+  }
+
+  @Put(':id')
+  async update(@Body() body, @Param() params) {
+    return { body, params };
+  }
+
+  @Delete(':id')
+  async delete(@Param() params) {
+    return { params };
   }
 }
